c333b5ed1392d266c7f25b13eb01d8327c019076
Song Xu <song.xu.ucla@gmail.com>
2019-01-02 12:40:40 -0800

    Adding problem descriptions

--- Array/Sums/P170_TwoSumIII.py
+++ Array/Sums/P170_TwoSumIII.py
@@ -1,5 +1,23 @@
 
 '''
+Design and implement a TwoSum class. It should support the following operations: add and find.
+
+add - Add the number to an internal data structure.
+find - Find if there exists any pair of numbers which sum is equal to the value.
+
+Example 1:
+
+add(1); add(3); add(5);
+find(4) -> true
+find(7) -> false
+Example 2:
+
+add(3); add(1); add(2);
+find(3) -> true
+find(6) -> false
+
+
+
 
 Method 1: Old method, based on TwoSum (Return the
 unique true pair's INDs).
--- Array/Sums/P653_TwoSumIV.py
+++ Array/Sums/P653_TwoSumIV.py
@@ -1,5 +1,30 @@
 
 '''
+Given a Binary Search Tree and a target number, return true if there exist two elements in the BST such that their sum is equal to the given target.
+
+Example 1:
+Input:
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Target = 9
+
+Output: True
+Example 2:
+Input:
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Target = 28
+
+Output: False
+
 
 Thought will be boring to pre-order, and print, and use dict. O(n) time + O(n) space.
 
