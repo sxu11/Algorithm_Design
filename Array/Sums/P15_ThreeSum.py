5dcda56ce9c1cde40ad85efbbfab5cb883aa2eb9
Song Xu <song.xu.ucla@gmail.com>
2019-03-12 11:51:39 -0700

    ThreeSum revisit

--- Array/Sums/P15_ThreeSum.py
+++ Array/Sums/P15_ThreeSum.py
@@ -37,76 +37,99 @@ in step 0.2.
 
 '''
 
+# class Solution(object):
+#
+#     def twoSum_extended(self, nums, target, method):
+#
+#         all_res = []
+#
+#         i = 0
+#         j = len(nums) - 1
+#         while i < j:
+#             if nums[i] + nums[j] == target:
+#                 all_res.append([nums[i],nums[j]])
+#
+#                 # Remove redundancy II
+#                 if method == 1.2:
+#                     while nums[i] == all_res[-1][0] and i < j:
+#                         i += 1
+#                     while nums[j] == all_res[-1][1] and i < j:
+#                         j -= 1
+#                 else:
+#                     i += 1
+#                     j -= 1
+#             elif nums[i] + nums[j] < target:
+#                 i += 1
+#             else:
+#                 j -= 1
+#
+#         # deduplicate here!
+#
+#         if method == 1.1:
+#             new_res = []
+#             for res in all_res:
+#                 if res not in new_res:
+#                     new_res.append(res)
+#         else:
+#             new_res = all_res
+#
+#         return new_res
+#
+#     def threeSum(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: List[List[int]]
+#         """
+#         ress = []
+#
+#         method = 1.1
+#
+#
+#         nums_origin = nums[:]
+#         nums = sorted(nums)
+#
+#         for i in range(len(nums)):
+#
+#             if method > 1:
+#                 if i>0 and nums[i]==nums[i-1]:
+#                     continue
+#
+#             cur_nums = nums[i+1:]
+#             target = -nums[i]
+#
+#             cur_all_res = self.twoSum_extended(cur_nums, target, method)
+#             for one_res in cur_all_res:
+#                 one_res = [nums[i]] + one_res
+#                 ress.append(one_res)
+#
+#         if method == 0:
+#             ress_new = []
+#             for res in ress:
+#                 if res not in ress_new:
+#                     ress_new.append(res)
+#
+#         nums = nums_origin
+#         return ress
+
 class Solution(object):
-
-    def twoSum_extended(self, nums, target, method):
-
-        all_res = []
-
-        i = 0
-        j = len(nums) - 1
-        while i < j:
-            if nums[i] + nums[j] == target:
-                all_res.append([nums[i],nums[j]])
-
-                # Remove redundancy II
-                if method == 1.2:
-                    while nums[i] == all_res[-1][0] and i < j:
-                        i += 1
-                    while nums[j] == all_res[-1][1] and i < j:
-                        j -= 1
-                else:
-                    i += 1
-                    j -= 1
-            elif nums[i] + nums[j] < target:
-                i += 1
-            else:
-                j -= 1
-
-        # deduplicate here!
-
-        if method == 1.1:
-            new_res = []
-            for res in all_res:
-                if res not in new_res:
-                    new_res.append(res)
-        else:
-            new_res = all_res
-
-        return new_res
-
     def threeSum(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        ress = []
 
-        method = 1.1
+        def twoSum(target, array):
+            seen = set()
+            res = []
+            for num in array:
+                if target - num in seen:
+                    res.append(sorted([target - num, num, -target]))
+                seen.add(num)
+            return res
 
-
-        nums_origin = nums[:]
-        nums = sorted(nums)
-
+        final_res_set = set()
         for i in range(len(nums)):
-
-            if method > 1:
-                if i>0 and nums[i]==nums[i-1]:
-                    continue
-
-            cur_nums = nums[i+1:]
-            target = -nums[i]
-
-            cur_all_res = self.twoSum_extended(cur_nums, target, method)
-            for one_res in cur_all_res:
-                one_res = [nums[i]] + one_res
-                ress.append(one_res)
-
-        if method == 0:
-            ress_new = []
-            for res in ress:
-                if res not in ress_new:
-                    ress_new.append(res)
-
-        nums = nums_origin
-        return ress
+            cur_res = twoSum(-nums[i], nums[i + 1:])
+            for pair in cur_res:
+                final_res_set.add(tuple(pair))
+        return list(final_res_set)
