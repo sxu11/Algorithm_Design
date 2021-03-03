3bb968bc2fc54e5c3d32b227620c3fc995c3e73e
songxu <songxu@microsoft.com>
2021-03-02 21:15:52 -0800

    concurrent 2

---
+++ Daily/20210303_1115_PrintFooBarAlternately.py
@@ -0,0 +1,39 @@
+using
+System.Threading;
+
+public
+
+
+class FooBar {
+private int n;
+private AutoResetEvent event1;
+private AutoResetEvent event2;
+
+public FooBar(int n) {
+this.n = n;
+event1 = new AutoResetEvent(false);
+event2 = new AutoResetEvent(true);
+}
+
+public void Foo(Action printFoo) {
+
+for (int i = 0; i < n; i++) {
+
+// printFoo() outputs "foo".Do not change or remove this line.
+event2.WaitOne();
+printFoo();
+event1.Set();
+}
+}
+
+public void Bar(Action printBar) {
+
+for (int i = 0; i < n; i++) {
+
+// printBar() outputs "bar".Do not change or remove this line.
+event1.WaitOne();
+printBar();
+event2.Set();
+}
+}
+}
