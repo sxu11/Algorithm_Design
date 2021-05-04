a8490f3d964ed40d2eaa8287588ae2e8b3597063
songxu <songxu@microsoft.com>
2021-05-04 06:36:28 -0700

    word break2 better commented

--- Facebook/P140_WordBreakII.py
+++ Facebook/P140_WordBreakII.py
@@ -16,30 +16,33 @@ class Solution(object):
 
         def getBreakableTuples(ss):
             if ss in cached:
+                """only in this case (ss processed), we don't need proceed"""
                 return cached[ss]
-            # elif ss in wordDict:
-            # cached[ss] = [(ss,)]
             if ss in wordDict:
+                """even in this case (ss self in), we need see if substr in
+                for e.g. even if 'aa' was in dict, maybe 'a' was also in!"""
                 cached[ss] = [(ss,)]
             else:
                 cached[ss] = []
-            # if ss not in cached:
-            # cached[ss] = []
-            for i in range(len(ss)):
-                ss1 = ss[:i]
-                ss2 = ss[i:]
-                tuple1s = getBreakableTuples(ss1)
-                tuple2s = getBreakableTuples(ss2)
-                if tuple1s and tuple2s:
-                    res = crossCombineValues(tuple1s, tuple2s)
-                    cached[ss] += res
+            for i in range(len(ss)): """here, don't need to worry about i=0,
+            because once ss cached, won't do repeated processing
+                BUT still need to handle above 'ss in dict' case, which is 
+            actually the only place where we do the core logic: in dict?
+            """
+            ss1 = ss[:i]
+            ss2 = ss[i:]
+            tuple1s = getBreakableTuples(ss1)
+            tuple2s = getBreakableTuples(ss2)
+            if tuple1s and tuple2s:
+                res = crossCombineValues(tuple1s, tuple2s)
+                cached[ss] += res
 
-            return cached[ss]
+        return cached[ss]
 
-        getBreakableTuples(s)
+    getBreakableTuples(s)
 
-        resInTuples = cached[s]
-        res = []
-        for tuple1 in resInTuples:
-            res.append(' '.join(tuple1))
-        return list(set(res))
+    resInTuples = cached[s]
+    res = []
+    for tuple1 in resInTuples:
+        res.append(' '.join(tuple1))
+    return list(set(res))
